070a2b1756ba98e5e2c97d3d4e9378f2ca7cde8a
NING <ucqbnw4@ucl.ac.uk>
2018-01-12 00:23:07 +0000

    homework
    4

---
+++ Assets/Scipts/Homework/Homework4.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homework4 : MonoBehaviour 
+{
+    public GameObject popcorn;
+    IEnumerator NingCouroutine;
+
+    public Renderer poprend;
+    IEnumerator NingColorCouroutine;
+
+	// Use this for initialization
+	void Start () 
+    {
+        NingCouroutine = popbomb();
+        NingColorCouroutine = changePopcornColor();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+
+    {
+		
+	}
+}
